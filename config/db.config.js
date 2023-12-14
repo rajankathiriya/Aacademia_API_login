63a2103f5661d3adf5dc5845ce6c8b7a60e88fd7
ravi senjaliya <shitalacademy123@gmail.com>
2023-12-14 21:46:10 +0530

    chnage

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 //mongo DB create database(collection)
 module.exports = {
   // url: "mongodb+srv://pyramidd123:JqsIBB2eyt1IUgy3@cluster0.qdargx1.mongodb.net/shitalacademy"
-  url: "mongodb://127.0.0.1:27017/academia",
+  url: "mongodb+srv://ravisenjaliya008:raviraviravi@cluster0.dtnkpre.mongodb.net/academiaLogin",
 };
